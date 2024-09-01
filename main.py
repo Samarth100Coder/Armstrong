29c8ae325e04b7e75fc810cef3fe6392fa2b7734
Samarth100Coder <kumarsamatrh272@gmail.com>
2024-09-01 16:00:02 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+n=int(input('Enter a number: '))
+sum=0
+temp=n
+length=len(str(n))
+while temp>0:
+    digit=temp%10
+    sum=sum+digit**length
+    temp=temp//10
+    
+if sum==n:
+    print("It is an armstrong")
+else:
+    print("It is not an armstrong")
